8fe8672339ee138ce24c7312ce10014f983b51c9
DarkDisaters <2361629815@qq.com>
2019-11-13 11:26:33 +0800

    font

---
+++ todolearn1/src/views/HomeFontColor.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>This is homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est asperiores ut facere similique alias aliquid possimus officia quisquam minus libero. Vero commodi recusandae voluptates ex minus voluptatum. Minus, esse at!</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate dolorum natus excepturi animi aut atque sit repudiandae veritatis quis, amet qui pariatur maiores reiciendis, totam libero praesentium dolore porro nisi?</p>
+    <p class='display-4'>Massive Display</p>
+    <p class="display-1">Smaller Display</p>
+    <p class="headline">This is a headline</p>
+    <p class="subheading font-weight-bold">This is a subheading</p>
+  </div>
+</template>
+
+<script>
+
+</script>
